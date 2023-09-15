8044e23ef0e602ac9a05e4234fba115f6c2feb83
rhuancavalcanti <110117224+rhuancavalcanti@users.noreply.github.com>
2023-09-15 08:07:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 SABREDELUZ = () => alert("Estrela da Morte");
+mensagem = (mgs) => console.log(mgs);
+Soma =(a,b)=> a + b; 
+sub =(a,b)=> a - b; 
+mult =(a,b)=> a * b; 
+div =(a,b)=> a / b; 
+juros_simples = (c,j) => mult(c,div(j,100));
+montante_juros_simlples = (c,j) => soma(juros_simples(c,j),c);
+
+mensagem(soma(15,soma(11,soma(4,soma))));
+
+mensagem(sub(18,7));
+
+mensagen(soma(2,div(2,2)));
+
+mensagem(juros_simples(420,18));
