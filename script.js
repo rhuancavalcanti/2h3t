a8452be609033edffb862c9521d0288fcb66c213
2H-Gastao Gabriel Felipe Fernandes Ribeiro <108344738+gabriel24felipe@users.noreply.github.com>
2023-10-06 08:01:01 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,46 @@
-SABREDELUZ = () => alert("Estrela da Morte");
-mensagem = (mgs) => console.log(mgs);
-Soma =(a,b)=> a + b; 
-sub =(a,b)=> a - b; 
-mult =(a,b)=> a * b; 
-div =(a,b)=> a / b; 
+let num1 = "";
+let num2 = "";
+let valor = "";
+let temPonto = false;
+let executa = ""
+mensagem = (msg) => console.log(msg);
+soma = (a,b) => a + b;
+sub = (a,b) => a - b;
+mult = (a,b) => a * b;
+div = (a,b) => a / b;
 juros_simples = (c,j) => mult(c,div(j,100));
-montante_juros_simlples = (c,j) => soma(juros_simples(c,j),c);
+montante_juro_simples = (c,j) => soma(juros_simples(c,j),c);
 
-mensagem(soma(15,soma(11,soma(4,soma))));
-
-mensagem(sub(18,7));
-
-mensagen(soma(2,div(2,2)));
-
-mensagem(juros_simples(420,18));
-
-mostrar_diplay = (msg)=>{
-    document.getElementById("reultado").value = msg;
+mostrar_display = (msg)=>{
+  document.getElementById("resultado").value = msg;
 }
 
 function digitando(tecla){
-   if(tecla == "_"){
-   if(!temPonto){
-
-    
-   }
-
-   }
-   
-   
-   
-    valor += tecla;
-    mostrar_diplay(valor);
+  if(tecla == "."){
+    if(!temPonto){
+      valor += tecla;
+      mostrar_display(valor);
+      temPonto = true;
+    }
+    return;
+  }
+   valor += tecla;
+   mostrar_display(valor);
+}
+function operacao(op){
+  executa = op;
+  num1 = valor;
+}
+function calcula(){
+  if(executa != ""){
+    if(executa == "soma") mostrar__display(soma(num1, num2));
+    if(executa == "sub") mostrar__display(sub(num1, num2));
+    if(executa == "mult") mostrar__display(mult(num1, num2));
+    if(executa == "div") mostrar__display(div(num1, num2));
+ num1="";
+ num2="";
+ valor="";
+ temPonto=false;
+ executa="";
+  }
 }
